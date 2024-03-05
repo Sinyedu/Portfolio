df862a740c24c8f0b40dc45c9283afc535db36bf
Simon Nyblom <simnyb01@easv365.dk>
2024-03-05 16:52:59 +0100

    darkmode

--- CSS/app.js
+++ CSS/app.js
@@ -1,20 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var popup = document.getElementById('popup');
-    var lightModeBtn = document.getElementById('lightModeBtn');
-    var darkModeBtn = document.getElementById('darkModeBtn');
+// document.addEventListener('DOMContentLoaded', function() {
+//     var popup = document.getElementById('popup');
+//     var lightModeBtn = document.getElementById('lightModeBtn');
+//     var darkModeBtn = document.getElementById('darkModeBtn');
   
-    lightModeBtn.addEventListener('click', function() {
-      document.body.classList.remove('dark-mode');
-      document.body.classList.add('light-mode');
-      popup.classList.remove('show');
-    });
+//     lightModeBtn.addEventListener('click', function() {
+//       document.body.classList.remove('dark-mode');
+//       document.body.classList.add('light-mode');
+//       popup.classList.remove('show');
+//     });
   
-    darkModeBtn.addEventListener('click', function() {
-      document.body.classList.remove('light-mode');
-      document.body.classList.add('dark-mode');
-      popup.classList.remove('show');
-    });
-    popup.classList.add('show');
-  });
-
-
+//     darkModeBtn.addEventListener('click', function() {
+//       document.body.classList.remove('light-mode');
+//       document.body.classList.add('dark-mode');
+//       popup.classList.remove('show');
+//     });
+//     popup.classList.add('show');
+//   });
+// For future use!
